9f038e6bd935047325102763bc65d7d8512b0874
unknown <vijaychaurasiya911@gmail.com>
2023-09-28 20:01:50 +0530

    merged shipment and status table

--- demo/src/main/java/com/minibay/app/entity/Shipment.java
+++ demo/src/main/java/com/minibay/app/entity/Shipment.java
@@ -22,17 +22,37 @@ public class Shipment {
 
     private String referenceNo;
     
+    private String status;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date statusDateTime;
+
+    private String statusType;
+
+    private String statusLocation;
+
+    private String instructions;
+    
+    
+    
     public Shipment(){
     	
     }
 
-	public Shipment(String awb, Date pickUpDate, String nslCode, String sortCode, String referenceNo) {
+
+	public Shipment(String awb, Date pickUpDate, String nslCode, String sortCode, String referenceNo, String status,
+			Date statusDateTime, String statusType, String statusLocation, String instructions) {
 		super();
 		this.awb = awb;
 		this.pickUpDate = pickUpDate;
 		this.nslCode = nslCode;
 		this.sortCode = sortCode;
 		this.referenceNo = referenceNo;
+		this.status = status;
+		this.statusDateTime = statusDateTime;
+		this.statusType = statusType;
+		this.statusLocation = statusLocation;
+		this.instructions = instructions;
 	}
 
 	public String getAwb() {
@@ -75,10 +95,62 @@ public class Shipment {
 		this.referenceNo = referenceNo;
 	}
 
+	public String getStatus() {
+		return status;
+	}
+
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+
+	public Date getStatusDateTime() {
+		return statusDateTime;
+	}
+
+
+	public void setStatusDateTime(Date statusDateTime) {
+		this.statusDateTime = statusDateTime;
+	}
+
+
+	public String getStatusType() {
+		return statusType;
+	}
+
+
+	public void setStatusType(String statusType) {
+		this.statusType = statusType;
+	}
+
+
+	public String getStatusLocation() {
+		return statusLocation;
+	}
+
+
+	public void setStatusLocation(String statusLocation) {
+		this.statusLocation = statusLocation;
+	}
+
+
+	public String getInstructions() {
+		return instructions;
+	}
+
+
+	public void setInstructions(String instructions) {
+		this.instructions = instructions;
+	}
+
+
 	@Override
 	public String toString() {
 		return "Shipment [awb=" + awb + ", pickUpDate=" + pickUpDate + ", nslCode=" + nslCode + ", sortCode=" + sortCode
-				+ ", referenceNo=" + referenceNo + "]";
+				+ ", referenceNo=" + referenceNo + ", status=" + status + ", statusDateTime=" + statusDateTime
+				+ ", statusType=" + statusType + ", statusLocation=" + statusLocation + ", instructions=" + instructions
+				+ "]";
 	}
 
 	
